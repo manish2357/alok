ce439bca3665d6b0b8e2ff7378a2149bd6d0c870
alok135 <alok13.in@gmail.com>
2018-03-30 11:35:56 +0530

    alok

---
+++ src/main/java/alokops/manishhello/App.java
@@ -0,0 +1,13 @@
+package alokops.manishhello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi manish how are you" );
+    }
+}
